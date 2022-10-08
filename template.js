5cd5d3d8d933848ea3086bebac28037089314989
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 15:40:55 +0600

    added Template

--- template.js
+++ template.js
@@ -1,5 +1,9 @@
-// P:01
+const name = `Md. Abu Sayed`;
+const fatherName = `Mohammad Shah Alom`;
+const motherName = `Mst. Taslime Khanom`;
+const botherName = `Abdullah Sayef`;
+const isMarried = false;
 
-
-// P:02
-// P:03
+const myDetails = `My Name is ${name}.
+my father Name is ${fatherName},  my Mother Name is ${motherName} and my bother Name is ${botherName}. Now SAYED is married ? => ${isMarried}`;
+console.log(myDetails);
