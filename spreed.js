c94d160dae80cceae63d0dd5b6408cc71dffb041
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 15:32:43 +0600

    use spread operator in an aray

--- spreed.js
+++ spreed.js
@@ -1,5 +1,12 @@
 // P:01
-
-
+const odd = [1, 3, 5];
+const combined = [2, 4, 6, ...odd];
+console.log(combined);
 // P:02
+const even = [2, 4, 6, 8];
+const combinedNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, ...even];
+console.log(combinedNumbers);
 // P:03
+const array1 = [1, 2, 3, 4];
+const array2 = [...array1, 5, 6, 7, 8, 9];
+console.log(array2);
