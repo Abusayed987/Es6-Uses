b51ac7f64c9e613aaabf8ab3914bc72509434415
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 18:28:09 +0600

    find one in an array or object

--- find.js
+++ find.js
@@ -1,5 +1,17 @@
-// P:01
+const numbers = [12, 23, 65, 87, 89, 90, 86, 6543, 32];
+const evenFirstOne = numbers.find( num => num%2 === 0)
+// console.log(evenFirstOne);
 
-
-// P:02
-// P:03
+// p:02
+const products = [
+    { id: 01, name: 'laptop', price: 100 },
+    { id: 02, name: 'laptop', price: 500 },
+    { id: 03, name: 'laptop', price: 1000 },
+    { id: 04, name: 'laptop', price: 1500 },
+];
+const cepRateLaptop = products.find(laptop => laptop.price > 100);
+console.log(cepRateLaptop);
+// p:03
+const allNumbers = [12, 23, 65, 87, 89, 90, 86, 6543, 32];
+const dividedBy5 = allNumbers.find( num => num%5 === 0);
+// console.log(dividedBy5);
