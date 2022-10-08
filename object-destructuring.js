ac12c2251dcebcf322e6d8a6cda1baa8d0cdacdd
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 15:00:23 +0600

    added object Destructuring

--- object-destructuring.js
+++ object-destructuring.js
@@ -1,5 +1,28 @@
 // P:01
-
-
+const obj = {
+    name: 'sayed',
+    age: 20,
+    id: 2,
+    level: 10,
+}
+const { name, age, id, level } = obj;
+console.log(age);
+console.log(level);
 // P:02
+const mobilr = {
+    book: `al Quran`,
+    mobile: 'xiomi',
+    laptop: 'macbook',
+    relegion: 'islam'
+}
+const { book, relegion } = mobilr; // object er somay same name tai dite hobe
+console.log(relegion);
 // P:03
+const medicine = {
+    medicineType: 'Homio',
+    cost : 2500,
+    totalMedecineNumber : 6,
+    khaiteHobe : `3 mash`,
+}
+const {khaiteHobe, medicineType} = medicine;
+console.log(khaiteHobe, medicineType);
