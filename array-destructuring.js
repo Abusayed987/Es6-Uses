65cd6079c6bba98c53c238753be3fefe22020f9c
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 14:40:01 +0600

    added array destructuring

---
+++ array-destructuring.js
@@ -0,0 +1,12 @@
+// P:01
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 8, 9];
+// const [a, b, c, ...z] = array;
+// console.log(a, b, c, z);
+// P:02
+const array2 = [11, 22, 33, 44, 55, 66, 77, 88, 99];
+// const [x,y]= array2;
+
+// P:03
+const array3 = [99, 88, 77, 66, 55, 44, 33, 11];
+const [m,n,q,...z] = array3;
+console.log(z);
