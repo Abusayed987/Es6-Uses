f560d28128394e0c263ee5dcb391cf585b1a1507
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 17:21:57 +0600

    Run  for Each

--- array-forEach.js
+++ array-forEach.js
@@ -1,5 +1,11 @@
-// P:01
+// p:01
+const friends = ['Mokbul', 'Mamim', 'Mobbar', 'Modu mollah', 'Mifat', 'Majib'];
+const friend = friends.forEach(friend=> console.log(friend.length))
 
-
-// P:02
-// P:03
+const products = [
+    { id: 01, name: 'laptop', price: 121212 },
+    { id: 01, name: 'laptop', price: 121212 },
+    { id: 01, name: 'laptop', price: 121212 },
+    { id: 01, name: 'laptop', price: 121212 },
+]
+const product = products.forEach(product => console.log(product.price));
