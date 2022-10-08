6ad2980fbda30c0148b567868a0f22b13f6b3b69
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 15:12:54 +0600

    added default perameter declare

--- default-pera.js
+++ default-pera.js
@@ -1,5 +1,20 @@
 // P:01
-
-
+function defaultPera(x = 1, y = 1) {
+    const multiplyX = x * x;
+    const multiplyY = y*y ;
+    const sum = multiplyX+ multiplyY;
+    return sum;
+}
+const result =  defaultPera(10);  // 101
+console.log(result);
 // P:02
+function say(message = `hello World`){
+    return message;
+}
+console.log(say(`Welcome to Our new Project`));
 // P:03
+function showSum(number1 = 0, number2 = 1){
+    return number1 + number2;
+}
+const showResult = showSum(100+ 00); //100
+console.log(showResult);
