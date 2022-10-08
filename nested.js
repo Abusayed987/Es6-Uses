00242a2baeccff1aa97ffaf8ce49d3a60540a7ff
Md.Abu Sayed <sayedshishir.abu@gmail.com>
2022-10-08 16:11:10 +0600

    nested chain in array and object

--- nested.js
+++ nested.js
@@ -1,5 +1,32 @@
 // P:01
+const user = [
+    { id: 1, name: 'abul', job: 'doctor' }
+]
+const userJob = user[0].job;
+console.log(userJob);
 
 
 // P:02
+const data = {
+    count: 5000,
+    data: [
+        { id: 01, job: 'work', userName: 'bablu' },
+        { id: 02, job: 'abolTablo', userName: 'sokomarRay' }
+    ]
+}
+const user2Job = data.data[1].job;
+console.log(user2Job);
+
 // P:03
+const users = {
+    id: 2323,
+    name: `abolTabol Sokomar ray `,
+    address: {
+        street: `35/A dhaka Bangladesh`,
+        PostOffice: `harinathpur`,
+        side: 'rightSide'
+    }
+}
+const userPostOffice = users.address.PostOffice;
+console.log(userPostOffice);
+
